3ad36deb028350881551e8a88e21e38d95d47b0f
Xtract <41217280+AnyMinorDeerPanda@users.noreply.github.com>
2021-12-07 00:44:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+var score = 0
+var totalScore = 0
+var clicks = 1
+var shop = false
+
+if (!localStorage.getItem('clicks')) 
+  localStorage.setItem('clicks', clicks)
+else
+  clicks = localStorage.getItem('clicks')
+
+if (!localStorage.getItem('score') || !localStorage.getItem('totalScore')) {
+  localStorage.setItem('score', score)
+  localStorage.setItem('totalScore', totalScore)
+} else {
+  score = localStorage.getItem('score')
+  totalScore = localStorage.getItem('totalScore')
+}
+
+document.getElementById('score').innerText = score
+document.getElementById('totalScore').innerText = totalScore
+
+function increment() {
+  totalScore = parseInt(totalScore) + parseInt(clicks)
+  score = parseInt(score) + parseInt(clicks)
+  
+  refreshValue('score', score)
+  refreshValue('totalScore', totalScore)
+}
+
+function refreshValue(id, value) {
+  localStorage.setItem(id, value)
+  document.getElementById(id).innerText = value
+}
+
+function reset() {
+  localStorage.clear()
+  location.reload()
+}
+
+function getClick(clickNum) {
+  if (!(score > clickNum * 10)) return;
+  score = parseInt(score) - (parseInt(clickNum) * 10)
+  
+  clicks++
+  localStorage.setItem('clicks', clicks)
+  refreshValue('score', score)
+}
+
+function openShop() {
+  document.getElementById('shop').style.top = 0 + "px"
+}
+
+function closeShop() {
+  document.getElementById('shop').style.top = -75 + "px"
+}
