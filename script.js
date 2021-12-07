73a1f2fd172d2a1c9a41c52241d30db272ca6b04
Xtract <41217280+AnyMinorDeerPanda@users.noreply.github.com>
2021-12-07 01:50:20 +0000

    added clickers

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 var score = 0
 var totalScore = 0
 var clicks = 1
+var clickers = 0
 var shop = false
 
-if (!localStorage.getItem('clicks')) 
-  localStorage.setItem('clicks', clicks)
-else
-  clicks = localStorage.getItem('clicks')
-
-if (!localStorage.getItem('score') || !localStorage.getItem('totalScore')) {
-  localStorage.setItem('score', score)
-  localStorage.setItem('totalScore', totalScore)
-} else {
-  score = localStorage.getItem('score')
-  totalScore = localStorage.getItem('totalScore')
-}
+clicks = getLocal('clicks', clicks)
+clickers = getLocal('clickers', clickers)
+score = getLocal('score', score)
+totalScore = getLocal('totalScore', totalScore)
 
 document.getElementById('score').innerText = score
 document.getElementById('totalScore').innerText = totalScore
 
-function increment() {
-  totalScore = parseInt(totalScore) + parseInt(clicks)
-  score = parseInt(score) + parseInt(clicks)
+function increment(number) {
+  totalScore = parseInt(totalScore) + parseInt(number)
+  score = parseInt(score) + parseInt(number)
   
   refreshValue('score', score)
   refreshValue('totalScore', totalScore)
+}
+
+
+function getLocal(id, value) {
+  if (!localStorage.getItem(id)) {
+    localStorage.setItem(id, value)
+    return value;
+  } else {
+    return localStorage.getItem(id);
+  }
 }
 
 function refreshValue(id, value) {
@@ -38,12 +43,20 @@ function reset() {
 }
 
 function getClick(clickNum) {
-  console.log(clickNum)
   if (!(score > clickNum * 10)) return;
   score = parseInt(score) - (parseInt(clickNum) * 10)
   
   clicks = parseInt(clicks) + parseInt(clickNum)
   localStorage.setItem('clicks', clicks)
+  refreshValue('score', score)
+}
+
+function getClicker(clickerNum) {
+  if (!(score > clickerNum * 100)) return;
+  score = parseInt(score) - (parseInt(clickerNum) * 100)
+  
+  clickers = parseInt(clickers) + parseInt(clickerNum)
+  localStorage.setItem('clickers', clickers)
   refreshValue('score', score)
 }
 
@@ -54,3 +67,7 @@ function openShop() {
 function closeShop() {
   document.getElementById('shop').style.top = -75 + "px"
 }
+
+setInterval(function() {
+  increment(clickers)
+}, 1000)
