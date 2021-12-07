66622b1ce86dc4d5ae20b2d2bb737fee49ef0bab
Xtract <41217280+AnyMinorDeerPanda@users.noreply.github.com>
2021-12-07 15:39:52 +0000

    better item system - clicker efficiency - mobile friendly

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-var score = 0
-var totalScore = 0
-var clicks = 1
-var clickers = 0
+var r = document.querySelector(':root');
+r.style.setProperty('--pos', -100 + '%')
+
+const stats = {
+  'clickerEfficiency': 0,
+  'totalScore': 0,
+  'score': 0,
+  'clickers': 0,
+  'clicks': 1
+}
+
+var clickersEnabled = true
 var shop = false
 
-clicks = getLocal('clicks', clicks)
-clickers = getLocal('clickers', clickers)
-score = getLocal('score', score)
-totalScore = getLocal('totalScore', totalScore)
+const stock = {
+  'clickerEfficiency': {
+    'price': 10000,
+    'max': 999
+  },
+  'clickers': {
+    'price': 100,
+    'max': 'inf'
+  },
+  'clicks': {
+    'price': 10,
+    'max': 'inf'
+  }
+}
+
+stats.clicks = getLocal('clicks', stats.clicks)
+stats.clickers = getLocal('clickers', stats.clickers)
+stats.clickerEfficiency = getLocal('efficiency', stats.clickerEfficiency)
+stats.score = getLocal('score', stats.score)
+stats.totalScore = getLocal('totalScore', stats.totalScore)
 
 document.getElementById('score').innerText = score
 document.getElementById('totalScore').innerText = totalScore
 
+fireClickers()
+
 function increment(number) {
-  totalScore = parseInt(totalScore) + parseInt(number)
-  score = parseInt(score) + parseInt(number)
+  stats.totalScore = parseInt(stats.totalScore) + parseInt(number)
+  stats.score = parseInt(stats.score) + parseInt(number)
   
-  refreshValue('score', score)
-  refreshValue('totalScore', totalScore)
+  refreshValue('score', stats.score)
+  refreshValue('totalScore', stats.totalScore)
 }
 
 
@@ -38,36 +64,34 @@ function refreshValue(id, value) {
 }
 
 function reset() {
+  clickersEnabled = false
   localStorage.clear()
   location.reload()
 }
 
-function getClick(clickNum) {
-  if (!(score > clickNum * 10)) return;
-  score = parseInt(score) - (parseInt(clickNum) * 10)
+function getItem(item, num) {
+  if (!(stock[item].max === 'inf') || stats[item] >= stock[item].max) return
+  if (!(stats.score >= num * stock[item].price)) return;
+  stats.score = parseInt(stats.score) - (parseInt(num) * stock[item].price)
   
-  clicks = parseInt(clicks) + parseInt(clickNum)
-  localStorage.setItem('clicks', clicks)
-  refreshValue('score', score)
-}
-
-function getClicker(clickerNum) {
-  if (!(score > clickerNum * 100)) return;
-  score = parseInt(score) - (parseInt(clickerNum) * 100)
-  
-  clickers = parseInt(clickers) + parseInt(clickerNum)
-  localStorage.setItem('clickers', clickers)
-  refreshValue('score', score)
+  stats[item] = parseInt(stats[item]) + parseInt(num)
+  localStorage.setItem(item, stats[item])
+  refreshValue('score', stats.score)
 }
 
 function openShop() {
-  document.getElementById('shop').style.top = 0 + "px"
+  r.style.setProperty('--pos', 0 + '%')
 }
 
 function closeShop() {
-  document.getElementById('shop').style.top = -75 + "px"
+  r.style.setProperty('--pos', -100 + '%')
 }
 
-setInterval(function() {
-  increment(clickers)
-}, 1000)
+function fireClickers () {
+  setTimeout(function() {
+    if (clickersEnabled)
+      increment(stats.clickers)
+
+    fireClickers()
+  }, 1000 - stats.clickerEfficiency)
+}
